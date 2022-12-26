b45b6a33945e1046d0d1b907ba8e7f4cb27d1518
Farhana Akter <110415773+farhana-95@users.noreply.github.com>
2022-12-26 22:56:18 +0600

    final

--- lib/Screens/Home/Profile/Settings/setting.dart
+++ lib/Screens/Home/Profile/Settings/setting.dart
@@ -1,13 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../constants.dart';
 
-class Setting extends StatelessWidget {
+class Setting extends StatefulWidget {
   const Setting({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  State<Setting> createState() => _SettingState();
+}
 
+class _SettingState extends State<Setting> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  late String  Id;
+  getCurrentUser() {
+    final User? user = _auth.currentUser;
+    final uid = user!.uid;
+    // Similarly we can get email as well
+    //final uemail = user.email;
+    //final uname = user.name;
+    //var text = Text('Mail: $uemail');
+    print(uid);
+    // _userInfo;
+    // print(_userInfo);
+    return uid;
+  }
+  late final CollectionReference _trip;
+  @override
+  void initState(){
+    Id=getCurrentUser();
+    _trip = FirebaseFirestore.instance.
+    collection("trip").doc(Id).collection("trip");
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Settings'),
@@ -56,6 +85,11 @@ class Setting extends StatelessWidget {
                   content: const Text('Reset Data'),
                   actions: [
                     TextButton(onPressed: (){
+                      _trip.get().then((snapshot) {
+                        for (DocumentSnapshot doc in snapshot.docs) {
+                          doc.reference.delete();
+                        }
+                      });
 
                     },
                         child: Text('OK')),
